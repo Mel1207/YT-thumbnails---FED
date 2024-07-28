dba51b83c2d0579869bff86a07c49773e11ee14f
Mel <57514727+Mel1207@users.noreply.github.com>
2024-07-29 03:29:14 +0800

    inject direct api connector

--- src/composables/getPlaylist1.js
+++ src/composables/getPlaylist1.js
@@ -1,11 +1,11 @@
 import { ref } from "vue"
 export const getPlaylist1 = () => {
   const videos = ref([])
-  const pl1 = import.meta.env.VITE_PL1_ID
-  const serviceAPI = import.meta.env.VITE_CRED_API_SINGLE
+  // const pl1 = import.meta.env.VITE_PL1_ID
+  // const serviceAPI = import.meta.env.VITE_CRED_API_SINGLE
 
   const getPlayList = () => {
-    fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C%20snippet&playlistId=${pl1}&key=${serviceAPI}`).then(res => res.json()).then(data => {
+    fetch(`https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails%2C%20snippet&playlistId=${import.meta.env.VITE_PL1_ID}&key=${import.meta.env.VITE_CRED_API_SINGLE}`).then(res => res.json()).then(data => {
       videos.value = data.items
     }).catch(err => console.log(err))
   }
